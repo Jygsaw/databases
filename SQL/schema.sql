50e0cc76db6ba5b397724e3b48c3778d2178e530
Hack Reactor Students <jygsaw@gmail.com>
2014-06-27 17:08:54 -0700

    Created database

--- SQL/schema.sql
+++ SQL/schema.sql
@@ -3,18 +3,35 @@ CREATE DATABASE chat;
 USE chat;
 
 CREATE TABLE messages (
-  /* Describe your table here.*/
+  id INT(7) NOT NULL AUTO_INCREMENT,
+  text VARCHAR(200) NOT NULL,
+  time DATETIME NOT NULL,
+  id_users INT(7) NOT NULL,
+  id_chatrooms INT(7) NOT NULL,
+  PRIMARY KEY (id)
 );
 
 /* Create other tables and define schemas for them here! */
 
+CREATE TABLE users (
+  id INT(7) NOT NULL AUTO_INCREMENT,
+  username VARCHAR(30) NOT NULL,
+  PRIMARY KEY (id)
+);
 
+CREATE TABLE chatrooms (
+  id INT(7) NOT NULL AUTO_INCREMENT,
+  roomname VARCHAR(30) NOT NULL,
+  PRIMARY KEY (id)
+);
 
+CREATE TABLE friends (
+  id INT(7) NOT NULL AUTO_INCREMENT,
+  id_friender INT(7) NOT NULL,
+  id_friendee INT(7) NOT NULL,
+  PRIMARY KEY (id)
+);
 
 /*  Execute this file from the command line by typing:
  *    mysql < schema.sql
  *  to create the database and the tables.*/
-
-
-
-
